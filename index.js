bad617c5031c8278e70a24b94e70a3bce02a66b6
Michael <micklekoltsov@gmail.com>
2021-03-27 15:33:58 +0300

    Server created

---
+++ index.js
@@ -0,0 +1,54 @@
+require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
+const createError = require('http-errors');
+const path = require('path');
+const MongoStore = require('connect-mongo');
+const dbConnect = require('./db/db');
+const mongoUrl = process.env.DATABASE_STRING;
+
+// Import routers
+const indexRouter = require('./routes/index');
+
+const app = express();
+
+dbConnect();
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect session
+app.use(
+  session({
+    secret: 'show me your favorite route',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false }, // maxAge: 60000
+    store: MongoStore.create({ mongoUrl }),
+  })
+);
+
+// Send user to all HBS
+app.use((req, res, next) => {
+  res.locals.username = req.session.username;
+  next();
+});
+
+// Routers
+app.use('/', indexRouter);
+
+// Catch errors if all of routers above has no responses
+app.use((req, res, next) => {
+  const error = createError(
+    404,
+    'Запрашиваемой страницы не существует на сервере.'
+  );
+  next(error);
+});
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log('Server has been started!')
+);
