44a5d593693cc19b9770863e120926255987caf8
Michael <micklekoltsov@gmail.com>
2021-05-14 09:30:06 +0300

    Small fix

--- public/js/client.js
+++ public/js/client.js
@@ -45,47 +45,6 @@ document.querySelector('#search').addEventListener('keyup', async (event) => {
   searchResultsBox.innerText += result.text;
 });
 
-// // Отображаем карту на странице приключения
-// if (document.querySelector('.adventure-card-map')) {
-//   const coordinatesText = document.querySelector('#coordinatesStorage').value;
-//   const coordinates = coordinatesText.split(',');
-//   const coordinatesFormatted = [];
-//   for (let i = 0; i < coordinates.length; i += 2) {
-//     coordinatesFormatted.push([coordinates[i], coordinates[i + 1]]);
-//   }
-
-//   const routePlanItemsTitles = document.querySelectorAll('.routePlanItemTitle');
-//   const routePlanItemsDescriptions = document.querySelectorAll(
-//     '.routePlanItemDescription'
-//   );
-
-//   ymaps.ready(() => {
-//     const myMap = new ymaps.Map(
-//       document.querySelector('.map'),
-//       {
-//         center: coordinatesFormatted[0],
-//         zoom: 10,
-//         controls: ['typeSelector', 'fullscreenControl', 'zoomControl'],
-//       },
-//       { buttonMaxWidth: 300, balloonMaxWidth: 200 }
-//     );
-
-//     for (let i = 0; i < coordinatesFormatted.length; i += 1) {
-//       myMap.geoObjects.add(
-//         new ymaps.Placemark(
-//           coordinatesFormatted[i],
-//           {
-//             balloonContent: `<b>${routePlanItemsTitles[i].innerText}</b><br>${routePlanItemsDescriptions[i].innerText}`,
-//           },
-//           {
-//             draggable: false,
-//           }
-//         )
-//       );
-//     }
-//   });
-// }
-
 // Проверки для форм
 const alertBox = document.querySelector('#alertBox');
 alertBox.setAttribute('style', 'display: none');
