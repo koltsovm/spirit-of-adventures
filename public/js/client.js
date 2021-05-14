9f3c11b0122769e03fac92a4514b81d5e1e6e2af
Michael <micklekoltsov@gmail.com>
2021-05-14 09:15:51 +0300

    Client scripts refactored

--- public/js/client.js
+++ public/js/client.js
@@ -45,122 +45,46 @@ document.querySelector('#search').addEventListener('keyup', async (event) => {
   searchResultsBox.innerText += result.text;
 });
 
-// Функция рендериинга карты
-function renderMap() {
-  document.querySelector('.form-map').innerHTML = '';
-  ymaps.ready(() => {
-    const myMap = new ymaps.Map(
-      document.querySelector('.map'),
-      {
-        center: [55.76, 37.64],
-        zoom: 12,
-        controls: [
-          'typeSelector',
-          'fullscreenControl',
-          'zoomControl',
-          'searchControl',
-        ],
-      },
-      {
-        buttonMaxWidth: 300,
-        balloonMaxWidth: 200,
-        searchControlProvider: 'yandex#search',
-      }
-    );
+// // Отображаем карту на странице приключения
+// if (document.querySelector('.adventure-card-map')) {
+//   const coordinatesText = document.querySelector('#coordinatesStorage').value;
+//   const coordinates = coordinatesText.split(',');
+//   const coordinatesFormatted = [];
+//   for (let i = 0; i < coordinates.length; i += 2) {
+//     coordinatesFormatted.push([coordinates[i], coordinates[i + 1]]);
+//   }
 
-    // eslint-disable-next-line prefer-destructuring
-    const geolocation = ymaps.geolocation;
+//   const routePlanItemsTitles = document.querySelectorAll('.routePlanItemTitle');
+//   const routePlanItemsDescriptions = document.querySelectorAll(
+//     '.routePlanItemDescription'
+//   );
 
-    geolocation
-      .get({
-        provider: 'yandex',
-        mapStateAutoApply: true,
-      })
-      .then((result) => {
-        result.geoObjects.get(0).properties.set({});
-        myMap.geoObjects.add(result.geoObjects);
-        // myMap.setCenter(result.geoObjects.position, 12);
-      });
+//   ymaps.ready(() => {
+//     const myMap = new ymaps.Map(
+//       document.querySelector('.map'),
+//       {
+//         center: coordinatesFormatted[0],
+//         zoom: 10,
+//         controls: ['typeSelector', 'fullscreenControl', 'zoomControl'],
+//       },
+//       { buttonMaxWidth: 300, balloonMaxWidth: 200 }
+//     );
 
-    // Отслеживаем щелчки по карте
-    myMap.events.add('click', (event) => {
-      if (!myMap.balloon.isOpen()) {
-        const coords = event.get('coords');
-        document
-          .getElementById('mapDataHidden')
-          .insertAdjacentHTML(
-            'beforeend',
-            `<input type="text" name="map" style="display: none" class="mapCoordinates" value="${coords}">`
-          );
-        myMap.geoObjects.add(
-          new ymaps.Placemark(
-            coords,
-            {
-              balloonContent: 'цвет <strong>воды пляжа бонди</strong>',
-            },
-            {
-              draggable: true,
-            }
-          )
-        );
-        // editor.startEditing();
-        // myMap.balloon.open(coords, {
-        //   contentHeader: 'Событие!',
-        //   contentBody:
-        //     `${'<p>Кто-то щелкнул по карте.</p>'
-        //     + '<p>Координаты щелчка: '}${
-        //       [coords[0].toPrecision(6), coords[1].toPrecision(6)].join(', ')
-        //     }</p>`,
-        //   contentFooter: '<sup>Щелкните еще раз</sup>',
-        // });
-      } else {
-        myMap.balloon.close();
-      }
-    });
-  });
-}
-
-// Отображаем карту на странице приключения
-if (document.querySelector('.adventure-card-map')) {
-  // renderMap();
-  const coordinatesText = document.querySelector('#coordinatesStorage').value;
-  const coordinates = coordinatesText.split(',');
-  const coordinatesFormatted = [];
-  for (let i = 0; i < coordinates.length; i += 2) {
-    coordinatesFormatted.push([coordinates[i], coordinates[i + 1]]);
-  }
-
-  const routePlanItemsTitles = document.querySelectorAll('.routePlanItemTitle');
-  const routePlanItemsDescriptions = document.querySelectorAll(
-    '.routePlanItemDescription'
-  );
-
-  ymaps.ready(() => {
-    const myMap = new ymaps.Map(
-      document.querySelector('.map'),
-      {
-        center: coordinatesFormatted[0],
-        zoom: 10,
-        controls: ['typeSelector', 'fullscreenControl', 'zoomControl'],
-      },
-      { buttonMaxWidth: 300, balloonMaxWidth: 200 }
-    );
-
-    for (let i = 0; i < coordinatesFormatted.length; i += 1) {
-      myMap.geoObjects.add(
-        new ymaps.Placemark(
-          coordinatesFormatted[i],
-          {
-            balloonContent: `<b>${routePlanItemsTitles[i].innerText}</b><br>${routePlanItemsDescriptions[i].innerText}`,
-          },
-          {
-            draggable: false,
-          }
-        )
-      );
-    }
-  });
-}
+//     for (let i = 0; i < coordinatesFormatted.length; i += 1) {
+//       myMap.geoObjects.add(
+//         new ymaps.Placemark(
+//           coordinatesFormatted[i],
+//           {
+//             balloonContent: `<b>${routePlanItemsTitles[i].innerText}</b><br>${routePlanItemsDescriptions[i].innerText}`,
+//           },
+//           {
+//             draggable: false,
+//           }
+//         )
+//       );
+//     }
+//   });
+// }
 
 // Проверки для форм
 const alertBox = document.querySelector('#alertBox');
@@ -254,176 +178,6 @@ document.addEventListener('click', async (event) => {
         document.querySelector('#alertMessage2').innerText =
           'Неверный логин и/или пароль';
       }
-    }
-  }
-
-  // Открыть форму создания приключения
-  if (
-    event.target.dataset.id === 'nav-create' &&
-    !document.querySelector('.trip-creation-form')
-  ) {
-    const fetchQuery = await fetch('/profile/create', {
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    const result = await fetchQuery.text();
-    document.querySelector('#nav-create').innerHTML = result;
-  }
-
-  // Открыть созданные приключения
-  if (event.target.dataset.id === 'nav-created') {
-    const fetchQuery = await fetch('/profile/created', {
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    const result = await fetchQuery.text();
-    document.querySelector('#nav-contact').innerHTML = result;
-  }
-
-  // Добавить путевую точку
-  if (event.target.dataset.id === 'addWaypoint') {
-    const waypointsList = document.querySelector('#waypointsList');
-    waypointsList.insertAdjacentHTML(
-      'beforeend',
-      `<div class="form-floating col-sm-9">
-        <div class="form-floating col-sm-9 mb-3 waypoint">
-          <div class="col-md-6 mb-3">
-            <label for="waypointTitle" class="form-label">Название точки  <sup style="color: red;">*</sup></label>
-            <input type="text" class="form-control waypointTitle" required>
-          </div>
-          <div class="form-floating mb-3">
-            <input type="text" class="form-control waypointDescription" placeholder='Например, "Городец за один день"' required>
-            <label>Описание <sup style="color: red;">*</sup></label>
-          </div>
-          <div class="form-floating mb-3">
-            <button type="button" data-id="deleteWaypoint" class="btn btn-outline-primary">Удалить точку</button>
-          </div>
-          <div class="row mb-3">
-        </div>
-        </div>
-      </div>`
-    );
-  }
-
-  // Отобразить карту
-  if (event.target.dataset.id === 'addMap') {
-    renderMap();
-  }
-
-  // Отправка формы, создание приключения
-  if (event.target.dataset.id === 'createAdventure') {
-    event.preventDefault();
-
-    // Заголовки путевых точек
-    const waypointTitles = document.querySelectorAll('.waypointTitle');
-    const waypointTitlesValues = [];
-    waypointTitles.forEach((el) => waypointTitlesValues.push(el.value));
-    // Описания путевых точек
-    const waypointDescriptions = document.querySelectorAll(
-      '.waypointDescription'
-    );
-    const waypointDescriptionsValues = [];
-    waypointDescriptions.forEach((el) =>
-      waypointDescriptionsValues.push(el.value)
-    );
-    // Собираем путевые точки с заголовками в один массив
-    const waypointsAll = [];
-    for (let i = 0; i < waypointTitlesValues.length; i += 1) {
-      waypointsAll.push([
-        waypointTitlesValues[i],
-        waypointDescriptionsValues[i],
-      ]);
-    }
-
-    const mapCoordinates = [];
-    document
-      .querySelectorAll('.mapCoordinates')
-      .forEach((el) => mapCoordinates.push(el.value.split(',')));
-
-    const titleInput = document.querySelector('#floatingInput');
-    const categoryInput = document.querySelector('#floatingSelect');
-    const descriptionInput = document.querySelector('#floatingTextarea2');
-
-    if (
-      !titleInput.value ||
-      !categoryInput.value ||
-      !descriptionInput.value ||
-      !waypointsAll.length
-    ) {
-      document.querySelector('#alertBox').removeAttribute('style');
-      document.querySelector('#alertMessage').innerText =
-        'Заполните обязательные поля!';
-    } else {
-      const fetchQuery = await fetch('/profile/create', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          title: titleInput.value,
-          category: categoryInput.value,
-          description: descriptionInput.value,
-          routePlan: waypointsAll,
-          coordinates: mapCoordinates,
-          // photos
-        }),
-      });
-
-      const result = await fetchQuery.text();
-      document.querySelector('#nav-create').innerHTML = result;
-    }
-  }
-
-  // Отправка формы, редактирование приключения
-  if (event.target.dataset.id === 'editAdventure') {
-    event.preventDefault();
-
-    // Заголовки путевых точек
-    const waypointTitles = document.querySelectorAll('.waypointTitle');
-    const waypointTitlesValues = [];
-    waypointTitles.forEach((el) => waypointTitlesValues.push(el.value));
-    // Описания путевых точек
-    const waypointDescriptions = document.querySelectorAll(
-      '.waypointDescription'
-    );
-    const waypointDescriptionsValues = [];
-    waypointDescriptions.forEach((el) =>
-      waypointDescriptionsValues.push(el.value)
-    );
-    // Собираем путевые точки с заголовками в один массив
-    const waypointsAll = [];
-    for (let i = 0; i < waypointTitlesValues.length; i += 1) {
-      waypointsAll.push([
-        waypointTitlesValues[i],
-        waypointDescriptionsValues[i],
-      ]);
-    }
-
-    const titleInput = document.querySelector('#floatingInput');
-    const categoryInput = document.querySelector('#floatingSelect');
-    const descriptionInput = document.querySelector('#floatingTextarea2');
-
-    if (
-      !titleInput.value ||
-      !categoryInput.value ||
-      !descriptionInput.value ||
-      !waypointsAll.length
-    ) {
-      document.querySelector('#alertBox').removeAttribute('style');
-      document.querySelector('#alertMessage').innerText =
-        'Заполните обязательные поля!';
-    } else {
-      const fetchQuery = await fetch(`/category/owner/${event.target.dataset.adventure}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          title: titleInput.value,
-          category: categoryInput.value,
-          description: descriptionInput.value,
-          routePlan: waypointsAll,
-        }),
-      });
-
-      const result = await fetchQuery.text();
-      document.querySelector('.body-wrapper').innerHTML = result;
     }
   }
 });
--- public/js/profile.client.js
+++ public/js/profile.client.js
@@ -6,6 +6,7 @@ const avatarInput = document.getElementById('avatarFile');
 const avatarUploadButton = document.getElementById('avatar-upload');
 const spinnerButton = document.getElementById('btn-hidden');
 
+// Обновление аватара
 avatarEditButton.addEventListener('click', () => {
   myModal.show();
 });
@@ -33,6 +34,253 @@ avatarUploadButton.addEventListener('click', async (event) => {
     spinnerButton.hidden = true;
 
     myModal.hide();
+  }
+});
+
+// Функция рендериинга карты
+function renderMap() {
+  document.querySelector('.form-map').innerHTML = '';
+  ymaps.ready(() => {
+    const myMap = new ymaps.Map(
+      document.querySelector('.map'),
+      {
+        center: [55.76, 37.64],
+        zoom: 12,
+        controls: [
+          'typeSelector',
+          'fullscreenControl',
+          'zoomControl',
+          'searchControl',
+        ],
+      },
+      {
+        buttonMaxWidth: 300,
+        balloonMaxWidth: 200,
+        searchControlProvider: 'yandex#search',
+      }
+    );
+
+    // eslint-disable-next-line prefer-destructuring
+    const geolocation = ymaps.geolocation;
+
+    geolocation
+      .get({
+        provider: 'yandex',
+        mapStateAutoApply: true,
+      })
+      .then((result) => {
+        result.geoObjects.get(0).properties.set({});
+        myMap.geoObjects.add(result.geoObjects);
+        // myMap.setCenter(result.geoObjects.position, 12);
+      });
+
+    // Отслеживаем щелчки по карте
+    myMap.events.add('click', (event) => {
+      if (!myMap.balloon.isOpen()) {
+        const coords = event.get('coords');
+        document
+          .getElementById('mapDataHidden')
+          .insertAdjacentHTML(
+            'beforeend',
+            `<input type="text" name="map" style="display: none" class="mapCoordinates" value="${coords}">`
+          );
+        myMap.geoObjects.add(
+          new ymaps.Placemark(
+            coords,
+            {
+              balloonContent: 'цвет <strong>воды пляжа бонди</strong>',
+            },
+            {
+              draggable: true,
+            }
+          )
+        );
+        // editor.startEditing();
+        // myMap.balloon.open(coords, {
+        //   contentHeader: 'Событие!',
+        //   contentBody:
+        //     `${'<p>Кто-то щелкнул по карте.</p>'
+        //     + '<p>Координаты щелчка: '}${
+        //       [coords[0].toPrecision(6), coords[1].toPrecision(6)].join(', ')
+        //     }</p>`,
+        //   contentFooter: '<sup>Щелкните еще раз</sup>',
+        // });
+      } else {
+        myMap.balloon.close();
+      }
+    });
+  });
+}
+
+document.addEventListener('click', async (event) => {
+  // Открыть форму создания приключения
+  if (
+    event.target.dataset.id === 'nav-create' &&
+    !document.querySelector('.trip-creation-form')
+  ) {
+    const fetchQuery = await fetch('/profile/create', {
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const result = await fetchQuery.text();
+    document.querySelector('#nav-create').innerHTML = result;
+  }
+
+  // Открыть созданные приключения
+  if (event.target.dataset.id === 'nav-created') {
+    const fetchQuery = await fetch('/profile/created', {
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    const result = await fetchQuery.text();
+    document.querySelector('#nav-contact').innerHTML = result;
+  }
+
+  // Добавить путевую точку
+  if (event.target.dataset.id === 'addWaypoint') {
+    const waypointsList = document.querySelector('#waypointsList');
+    waypointsList.insertAdjacentHTML(
+      'beforeend',
+      `<div class="form-floating col-sm-9">
+        <div class="form-floating col-sm-9 mb-3 waypoint">
+          <div class="col-md-6 mb-3">
+            <label for="waypointTitle" class="form-label">Название точки  <sup style="color: red;">*</sup></label>
+            <input type="text" class="form-control waypointTitle" required>
+          </div>
+          <div class="form-floating mb-3">
+            <input type="text" class="form-control waypointDescription" placeholder='Например, "Городец за один день"' required>
+            <label>Описание <sup style="color: red;">*</sup></label>
+          </div>
+          <div class="form-floating mb-3">
+            <button type="button" data-id="deleteWaypoint" class="btn btn-outline-primary">Удалить точку</button>
+          </div>
+          <div class="row mb-3">
+        </div>
+        </div>
+      </div>`
+    );
+  }
+
+  // Отобразить карту
+  if (event.target.dataset.id === 'addMap') {
+    renderMap();
+  }
+
+  // Отправка формы, создание приключения
+  if (event.target.dataset.id === 'createAdventure') {
+    event.preventDefault();
+
+    // Заголовки путевых точек
+    const waypointTitles = document.querySelectorAll('.waypointTitle');
+    const waypointTitlesValues = [];
+    waypointTitles.forEach((el) => waypointTitlesValues.push(el.value));
+    // Описания путевых точек
+    const waypointDescriptions = document.querySelectorAll(
+      '.waypointDescription'
+    );
+    const waypointDescriptionsValues = [];
+    waypointDescriptions.forEach((el) =>
+      waypointDescriptionsValues.push(el.value)
+    );
+    // Собираем путевые точки с заголовками в один массив
+    const waypointsAll = [];
+    for (let i = 0; i < waypointTitlesValues.length; i += 1) {
+      waypointsAll.push([
+        waypointTitlesValues[i],
+        waypointDescriptionsValues[i],
+      ]);
+    }
+
+    const mapCoordinates = [];
+    document
+      .querySelectorAll('.mapCoordinates')
+      .forEach((el) => mapCoordinates.push(el.value.split(',')));
+
+    const titleInput = document.querySelector('#floatingInput');
+    const categoryInput = document.querySelector('#floatingSelect');
+    const descriptionInput = document.querySelector('#floatingTextarea2');
+
+    if (
+      !titleInput.value ||
+      !categoryInput.value ||
+      !descriptionInput.value ||
+      !waypointsAll.length
+    ) {
+      document.querySelector('#alertBox').removeAttribute('style');
+      document.querySelector('#alertMessage').innerText =
+        'Заполните обязательные поля!';
+    } else {
+      const fetchQuery = await fetch('/profile/create', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: titleInput.value,
+          category: categoryInput.value,
+          description: descriptionInput.value,
+          routePlan: waypointsAll,
+          coordinates: mapCoordinates,
+          // photos
+        }),
+      });
+
+      const result = await fetchQuery.text();
+      document.querySelector('#nav-create').innerHTML = result;
+    }
+  }
+
+  // Отправка формы, редактирование приключения
+  if (event.target.dataset.id === 'editAdventure') {
+    event.preventDefault();
+
+    // Заголовки путевых точек
+    const waypointTitles = document.querySelectorAll('.waypointTitle');
+    const waypointTitlesValues = [];
+    waypointTitles.forEach((el) => waypointTitlesValues.push(el.value));
+    // Описания путевых точек
+    const waypointDescriptions = document.querySelectorAll(
+      '.waypointDescription'
+    );
+    const waypointDescriptionsValues = [];
+    waypointDescriptions.forEach((el) =>
+      waypointDescriptionsValues.push(el.value)
+    );
+    // Собираем путевые точки с заголовками в один массив
+    const waypointsAll = [];
+    for (let i = 0; i < waypointTitlesValues.length; i += 1) {
+      waypointsAll.push([
+        waypointTitlesValues[i],
+        waypointDescriptionsValues[i],
+      ]);
+    }
+
+    const titleInput = document.querySelector('#floatingInput');
+    const categoryInput = document.querySelector('#floatingSelect');
+    const descriptionInput = document.querySelector('#floatingTextarea2');
+
+    if (
+      !titleInput.value ||
+      !categoryInput.value ||
+      !descriptionInput.value ||
+      !waypointsAll.length
+    ) {
+      document.querySelector('#alertBox').removeAttribute('style');
+      document.querySelector('#alertMessage').innerText =
+        'Заполните обязательные поля!';
+    } else {
+      const fetchQuery = await fetch(`/category/owner/${event.target.dataset.adventure}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          title: titleInput.value,
+          category: categoryInput.value,
+          description: descriptionInput.value,
+          routePlan: waypointsAll,
+        }),
+      });
+
+      const result = await fetchQuery.text();
+      document.querySelector('.body-wrapper').innerHTML = result;
+    }
   }
 });
 
